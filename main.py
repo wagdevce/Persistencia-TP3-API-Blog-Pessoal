5f812b754932e5bc5af5bc39e4b6b95fd645a04e
wagdevce <wmdigitalsolutions@gmail.com>
2025-08-06 22:20:03 -0300

    att

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ from app.core.db import (
     tag_collection,
     comment_collection,
 )
-
-# Importando nossos routers do sistema de Blog
 from app.routers.CategoryRouter import router as CategoryRouter
 from app.routers.PostRouter import router as PostRouter
 from app.routers.TagRouter import router as TagRouter
@@ -24,27 +22,17 @@ app = FastAPI(
     description="API para um sistema de gerenciamento de conteúdo de um blog.",
     version="1.3.0"
 )
-
-# --- INÍCIO DO CÓDIGO PARA CRIAR ÍNDICES ---
 @app.on_event("startup")
 async def create_indexes():
     """
     Esta função é executada na inicialização do app e garante
     que os índices essenciais existam no MongoDB.
     """
-    # Índice para buscar comentários por post rapidamente
     await comment_collection.create_index([("post_id", ASCENDING)])
-    
-    # Índices para buscar posts por categoria e tags
     await post_collection.create_index([("category_id", ASCENDING)])
-    await post_collection.create_index([("tags_id", ASCENDING)]) # MongoDB cria um "multikey index" para arrays
-
-    # Índice para ordenar posts por data de publicação
+    await post_collection.create_index([("tags_id", ASCENDING)]) 
     await post_collection.create_index([("publication_date", DESCENDING)])
 
-
-
-# Incluindo os novos routers na aplicação FastAPI
 app.include_router(UserRouter)
 app.include_router(CategoryRouter)
 app.include_router(PostRouter)
